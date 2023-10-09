47c84ca3ca745630767c4af53c5cda71414bebc2
shaolun77 <116580308+shaolun77@users.noreply.github.com>
2023-10-08 21:11:54 -0400

    initial commit

---
+++ recursion.js
@@ -0,0 +1,75 @@
+/** product: calculate the product of an array of numbers. */
+function product(nums) {
+  if (nums.length === 0) return 1;
+  return nums[0] * product(nums.slice(1));
+}
+
+/** longest: return the length of the longest word in an array of words. */
+function longest(words) {
+  if (words.length === 0) return 0;
+  return Math.max(words[0].length, longest(words.slice(1)));
+}
+
+/** everyOther: return a string with every other letter. */
+function everyOther(str, index = 0) {
+  if (index >= str.length) return "";
+  return str[index] + everyOther(str, index + 2);
+}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+function isPalindrome(str) {
+  if (str.length <= 1) return true;
+  if (str[0] !== str[str.length - 1]) return false;
+  return isPalindrome(str.slice(1, str.length - 1));
+}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+function findIndex(arr, val, index = 0) {
+  if (index >= arr.length) return -1;
+  if (arr[index] === val) return index;
+  return findIndex(arr, val, index + 1);
+}
+
+/** revString: return a copy of a string, but in reverse. */
+function revString(str) {
+  if (str === "") return str;
+  return revString(str.slice(1)) + str[0];
+}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+function gatherStrings(obj) {
+  let result = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      result.push(obj[key]);
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      result = result.concat(gatherStrings(obj[key]));
+    }
+  }
+
+  return result;
+}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+* return the index of that value (or -1 if val is not present). */
+function binarySearch(arr, val, low = 0, high = arr.length - 1) {
+  if (low > high) return -1;
+
+  const mid = Math.floor((low + high) / 2);
+
+  if (arr[mid] === val) return mid;
+  if (arr[mid] < val) return binarySearch(arr, val, mid + 1, high);
+  return binarySearch(arr, val, low, mid - 1);
+}
+
+module.exports = {
+  product,
+  longest,
+  everyOther,
+  isPalindrome,
+  findIndex,
+  revString,
+  gatherStrings,
+  binarySearch
+};
